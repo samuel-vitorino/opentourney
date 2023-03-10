d9b29ea4e5d6cf3806b2bdca37585e9d908a3b24
Samuel Vitorino <samvitorino@gmail.com>
2023-03-10 21:16:57 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# create-svelte
+
+Frontend for the OpenTourney app, powered by Svelte.
+
+## Developing
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of the app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { Spinner } from 'flowbite-svelte';
+</script>
+
+<div class="flex flex-col items-center self-center mt-20 mb-auto">
+    <img src="favicon.png" alt="OpenTourney Logo" class="w-[250px]">
+    <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to OpenTourney</h1>
+    <Spinner class="mt-10"/>
+</div>
