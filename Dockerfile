6124cb8fcb9dcf78d85cae55704a3d43c57f9be0
Samuel Vitorino <samvitorino@gmail.com>
2023-04-13 20:49:47 +0100

    Docker support

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+# Use the official Node.js runtime as a parent image
+FROM node:16 as build
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the package.json and package-lock.json files to the container
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy the rest of the application code to the container
+COPY . .
+
+# Build the SvelteKit application
+RUN npm run build
+
+# Use the official Nginx runtime as a parent image
+FROM nginx:1.21.3-alpine
+
+# Copy the nginx.conf file to the container
+COPY nginx.conf /etc/nginx/nginx.conf
+
+# Copy the built SvelteKit application to the Nginx default document root directory
+COPY --from=build /app/build /usr/share/nginx/html
+
+# Expose port 80 for Nginx to listen on
+EXPOSE 80
+
+# Start Nginx when the container starts
+CMD ["nginx", "-g", "daemon off;"]
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from "svelte-preprocess";
 import sass from 'sass';
-import adapter from "@sveltejs/adapter-auto";
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from "@sveltejs/kit/vite";
 import path from 'path';
 
@@ -22,7 +22,13 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html',
+      precompress: false,
+      strict: true
+    }),
     alias: {
       '@src': path.resolve('./src'),
       '@styles': path.resolve('./src/styles')
