5e1e5fb9a93fff23c615918d6624c367f4e8752a
Samuel Vitorino <samvitorino@gmail.com>
2023-06-23 20:52:15 +0100

    Pickem init

--- src/routes/matches/[id]/+page.svelte
+++ src/routes/matches/[id]/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
-    import { Tabs, TabItem, P, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+    import { Tabs, TabItem, P, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Button } from 'flowbite-svelte';
     import type { PageData } from './$types';
+    import { PUBLIC_WSURL } from "$env/static/public";
+    import { onMount } from "svelte";
+    import { Socket, io } from "socket.io-client";
     import { each } from 'svelte/internal';
+    import { userData } from '@src/stores/user';
     
     interface User {
         id: number;
@@ -38,10 +42,121 @@
         teams: Array<Team>;
     }
 
+    interface VetoStatus {
+        status: number;
+        matchType: number;
+        maps: string[];
+        team1Bans: string[];
+        team2Bans: string[];
+        team1Picks: string[];
+        team2Picks: string[];
+        finalized: boolean;
+        finalMap: string | null;
+    }
+
+    let currentVetoStatus: VetoStatus;
+    let socket: Socket;
+    
+    onMount(() => {
+        if (match.status == 0) {
+            socket = io(PUBLIC_WSURL, {});
+
+            socket.on("connect", () => {
+                console.log("Socket.IO connected");
+                socket.emit("joinMatchRoom", match.id);
+            });
+            
+            
+            socket.on("vetoStatus", (status: VetoStatus) => {
+                currentVetoStatus = status;
+                console.log(currentVetoStatus);
+            });
+        }
+    });
+
     export let data: PageData;
     let match = data.match as Match;
+    let imOwner: boolean;
+    let myTeam: number;
 
     match.score = [0, 0];
+
+    let breakNow = false;
+
+    $: {
+        for (let i = 0; i < match.teams.length; i++) {
+            for (let j = 0; j < match.teams[i].members.length; j++) {
+                if (match.teams[i].owner.id == $userData.id) {
+                    imOwner = true;
+                    myTeam = i;
+                    breakNow = true;
+                    break;
+                }
+            }
+
+            if (breakNow) {
+                break;
+            }
+        } 
+    }
+
+    let myTurn: boolean;
+
+    $: {
+        if (typeof imOwner !== undefined && typeof myTeam !== undefined && match && match.status == 0 && currentVetoStatus) {
+            if (imOwner && myTeam == 0 && (currentVetoStatus.status == 0 || currentVetoStatus.status == 1)) {
+                myTurn = true;
+            }
+            else if (imOwner && myTeam == 1 && (currentVetoStatus.status == 2 || currentVetoStatus.status == 3)) {
+                myTurn = true;
+            }
+            else {
+                myTurn = false;
+            }
+            console.log(myTurn, imOwner, myTeam, currentVetoStatus.status)
+        }
+    }
+
+    const sendPickBan = (action: string, map: string) => {
+        socket.emit("vetoPickBan", match.id, action, map);
+    }
+
+    const vetoStatusToString = (s: number, owner: boolean) => {
+        if (s == 0) {
+            if (owner && myTeam == 0) {
+                return "Your turn to ban a map"
+            } else {
+                return `Captain of team ${match.team_one_name} is banning`
+            }
+        } else if (s == 1) {
+            if (owner && myTeam == 0) {
+                return "Your turn to pick a map"
+            } else {
+                return `Captain of team ${match.team_one_name} is picking`
+            }
+        } else if (s == 2) {
+            if (owner && myTeam == 1) {
+                return "Your turn to ban a map"
+            } else {
+                return `Captain of team ${match.team_two_name} is banning`
+            }
+        } else if (s == 3) {
+            if (owner && myTeam == 1) {
+                return "Your turn to pick a map"
+            } else {
+                return `Captain of team ${match.team_two_name} is picking`
+            }
+        }
+    }
+
+    match.teams.forEach((t) => {
+        t.members.forEach((m) => {
+            if (m.id == $userData.id) {
+                imOwner = true;
+            }
+        })
+    })
+
     if (match.games) {
         match.games.forEach((g) => {
             if (g.team_one_score == 16) {
@@ -105,50 +220,36 @@
                             <div>
                                 <P weight="semibold">Waiting for match to start</P>
                             </div>
-                        {:else if match.status == 0}
-                            <div class="flex flex-col justify-between h-[400px]">
-                                <div class="flex flex-col w-[350px] h-[20px] bg-gray-200 box-content justify-center rounded-md p-4">
-                                    <div class="flex items-center">
-                                        <img src="/images/d2.jpg" class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
-                                        <P weight="semibold" size="sm">Dust 2</P>
+                        {:else if match.status == 0 && currentVetoStatus}
+                            <div class="flex flex-col justify-between items-center h-[430px] mb-5">
+                                {#if currentVetoStatus && currentVetoStatus.status != 4}
+                                    <P class="mb-3">{vetoStatusToString(currentVetoStatus.status, imOwner)}</P>
+                                {/if}
+                                {#each currentVetoStatus.maps as m, i}
+                                    <div class="flex w-[350px] h-[20px] bg-gray-200 box-content justify-between justify-center rounded-md p-4">
+                                        <div class="flex items-center">
+                                            <img src={`/images/${m}.jpg`} class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
+                                            <P weight="semibold" size="sm">{m}</P>
+                                        </div>
+                                        {#if currentVetoStatus.team1Bans.includes(m)}
+                                            <P>Banned by {match.team_one_name}</P>
+                                        {:else if currentVetoStatus.team2Bans.includes(m)}
+                                            <P>Banned by {match.team_two_name}</P>
+                                        {:else if currentVetoStatus.team1Picks.includes(m)}
+                                            <P>Picked by {match.team_two_name}</P>
+                                        {:else if currentVetoStatus.team2Picks.includes(m)}
+                                            <P>Picked by {match.team_two_name}</P>
+                                        {:else if currentVetoStatus.finalMap && currentVetoStatus.finalMap == m}
+                                            <P>Left out</P>
+                                        {:else if myTurn}
+                                            {#if currentVetoStatus.status == 0 || currentVetoStatus.status == 2}
+                                                <Button color="red" on:click={() => sendPickBan("ban", m)}>Ban</Button>
+                                            {:else if currentVetoStatus.status == 1 || currentVetoStatus.status == 3}
+                                                <Button color="green" on:click={() => sendPickBan("pick", m)}>Pick</Button>
+                                            {/if}
+                                        {/if}
                                     </div>
-                                </div>
-                                <div class="flex flex-col w-[350px] h-[20px] bg-gray-200 box-content justify-center rounded-md p-4">
-                                    <div class="flex items-center">
-                                        <img src="/images/mirage.jpg" class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
-                                        <P weight="semibold" size="sm">Mirage</P>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col w-[350px] h-[20px] bg-gray-200 box-content justify-center rounded-md p-4">
-                                    <div class="flex items-center">
-                                        <img src="/images/ancient.jpg" class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
-                                        <P weight="semibold" size="sm">Ancient</P>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col w-[350px] h-[20px] bg-gray-200 box-content justify-center rounded-md p-4">
-                                    <div class="flex items-center">
-                                        <img src="/images/vertigo.png" class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
-                                        <P weight="semibold" size="sm">Vertigo</P>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col w-[350px] h-[20px] bg-gray-200 box-content justify-center rounded-md p-4">
-                                    <div class="flex items-center">
-                                        <img src="/images/anubis.jpg" class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
-                                        <P weight="semibold" size="sm">Anubis</P>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col w-[350px] h-[20px] bg-gray-200 box-content justify-center rounded-md p-4">
-                                    <div class="flex items-center">
-                                        <img src="/images/overpass.jpg" class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
-                                        <P weight="semibold" size="sm">Overpass</P>
-                                    </div>
-                                </div>
-                                <div class="flex flex-col w-[350px] h-[20px] bg-gray-200 box-content justify-center rounded-md p-4">
-                                    <div class="flex items-center">
-                                        <img src="/images/nuke.jpg" class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
-                                        <P weight="semibold" size="sm">Nuke</P>
-                                    </div>
-                                </div>
+                                {/each}
                             </div>
                         {:else if match.status == 1}
                             <P weight="semibold" size="sm" class="mb-2">Ip address</P>
@@ -158,7 +259,7 @@
                             <P weight="semibold" size="sm" class="mt-5 mb-2">Map</P>
                             <div class="flex flex-col w-[350px] h-[20px] bg-gray-200 box-content justify-center rounded-md p-4">
                                 <div class="flex items-center">
-                                    <img src="/images/d2.jpg" class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
+                                    <img src="/images/Dust2.jpg" class="mr-5 w-[60px] h-[40px] rounded-md" alt="Server location flag">
                                     <P weight="semibold" size="sm">Dust_2</P>
                                 </div>
                             </div>
